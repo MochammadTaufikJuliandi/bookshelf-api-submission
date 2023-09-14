622986eb28890e231862a0ca88f0d9fb16b0ab26
Mochammad Taufik Juliandi <taufik.juliandi4@gmail.com>
2023-09-14 11:58:43 +0700

    firs commit

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+const {
+  addBooksHandler,
+  getAllBooksHandler,
+  getBooksByIdHandler,
+  editBookByIdHandler,
+  deleteBookById,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBooksByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookById,
+  },
+];
+
+module.exports = routes;
